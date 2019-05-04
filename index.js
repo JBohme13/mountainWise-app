2aadf257b9e9de7e940224a245884158a9abacbd
Jordan Bohme <jordan.M.Bohme@gmail.com>
2019-05-04 14:40:46 -0700

    finished initial request

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const googleApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+const geoCodeApiUrl = 'https://maps.googleapis.com/maps/api/geocode/json';
+const elevationsApiUrl = 'https://maps.googleapis.com/maps/api/elevation/json';
 const googleApiKey = 'AIzaSyD3Roxqxj05D5um1Or-TT1QoMRJ_U0VJVU'
 const openElevstionUrl = 'https://api.open-elevation.com/api/v1/lookup';
 const powderLinesUrl = 'http://api.powderlin.es/closest_stations';
 
-
-function formauQueryString(params) {
+function formatQueryString(params) {
     const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
     return queryItems.join('&');
 }
 
 function getResults() {
-    const searchValue = $('searchInput').val();
+    const searchValue = $('#searchInput').val();
     const params = {
         address: searchValue,
         key: googleApiKey
-    }
-    queryString = formanQueryString();
-    const url = googleApiUrl + '?' + queryString;
+    };
+    queryString = formatQueryString(params);
+    const url = geoCodeApiUrl + '?' + queryString;
+    console.log(url);
     fetch(url)
       .then(response => { 
         if (response.ok) {
             return response.json();
         } throw new Error(response.statusText)})
-      .then(responseJson => console.log(resonseJson))
+      .then(responseJson => console.log(responseJson))
       .catch(error => {
             $('.errorMessage').text(`Something went wrong: ${error.message}`);
         });
